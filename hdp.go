c7d7f78f9b65fda16fd049082e96756077c10722
vince rosso <vincer@vincer.net>
2016-07-05 14:16:28 -0700

    libhdplatinum moved to github.com/vincer

--- hdp.go
+++ hdp.go
@@ -3,7 +3,7 @@ package main
 import (
 	"os"
 	"github.com/codegangsta/cli"
-	"github.com/grepnull/libhdplatinum"
+	"github.com/vincer/libhdplatinum"
 	"github.com/olekukonko/tablewriter"
 	"strconv"
 )
